5e509ee47f6b39ba9b853ef8c0b04fafcef6a008
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-17 22:10:28 +0530

    Created

---
+++ EFConcurrencyCheckAttribute/models.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFConcurrencyCheckAttribute
+{
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        [ConcurrencyCheck]
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+
+
+}
